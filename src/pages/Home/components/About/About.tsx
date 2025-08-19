f7ae86c22641e8183295a25127cebede72c93a02
Hayden Westfall <westfallhayden@gmail.com>
2025-08-19 09:06:45 +0100

    Adds desktop about section to main page

--- src/pages/Home/components/About/About.tsx
+++ src/pages/Home/components/About/About.tsx
@@ -1,33 +1,43 @@
 import "./About.scss";
 import { CpiTag } from "../../../../components/cpiTag/CpiTag";
+import CpiButton from "../../../../components/cpiButton/CpiButton";
+import BaseballAccent from "../../../../assets/icons/baseball_accent.svg?react";
 
 export const About = () => {
   return (
     <>
-      <div className="about-wrapper">
+      <div className="about-section">
         <CpiTag index="01" label="ABOUT" />
-
-        <h2>
-          Let our experienced team elevate
-          <br /> your game to the next level
-        </h2>
-
-        <div className="stat-section">
-          <div className="stat-wrapper">
-            <span className="stat">200+</span>
-            <span className="label">Players Trained</span>
+        <div className="about-wrapper">
+          <div className="about-header">
+            <h2>
+              Let our experienced team elevate
+              <br /> your game to the next level
+            </h2>
+            <CpiButton label="Learn More" onClick={() => window.alert("Test")} className="cpi-button dark" />
           </div>
-          <div className="stat-wrapper">
-            <span className="stat">5+</span>
-            <span className="label">Years Training</span>
+
+          <div className="footer">
+            <div className="stat-section">
+              <div className="stat-wrapper">
+                <span className="stat">200+</span>
+                <span className="label">Players Trained</span>
+              </div>
+              <div className="stat-wrapper">
+                <span className="stat">5+</span>
+                <span className="label">Years Training</span>
+              </div>
+            </div>
+
+            <p>
+              At Conner Pohl Instruction, it’s about more than just reps. With years of experience as a D1 athlete and
+              varsity coach, Conner brings a passion for teaching and a sharp focus on mechanics to every session. From
+              young players to serious competitors, CPI helps athletes grow their game the right way.
+            </p>
           </div>
         </div>
 
-        <p>
-          At Conner Pohl Instruction, it’s about more than just reps. With years of experience as a D1 athlete and
-          varsity coach, Conner brings a passion for teaching and a sharp focus on mechanics to every session. From
-          young players to serious competitors, CPI helps athletes grow their game the right way.
-        </p>
+        <BaseballAccent className="baseball-accent" />
       </div>
     </>
   );
