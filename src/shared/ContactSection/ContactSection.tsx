8b6da3d8505eda1007aae86efa8a6c56fa013065
Hayden Westfall <westfallhayden@gmail.com>
2025-08-21 12:25:52 +0100

    Fixes contact section on desktop mode

--- src/shared/ContactSection/ContactSection.tsx
+++ src/shared/ContactSection/ContactSection.tsx
@@ -1,17 +1,31 @@
 import HittingImage from "../../assets/images/contact_hitting.png";
 import CpiButton from "../../components/cpiButton/CpiButton";
 import "./ContactSection.scss";
+import Testimony1 from "../../assets/images/testimony_1.png";
+import Testimony2 from "../../assets/images/testimony_2.png";
+import Testimony3 from "../../assets/images/testimony_3.png";
 
 export const ContactSection = () => {
   return (
     <>
       <div className="contact-section-wrapper">
-        <img src={HittingImage} alt="Player hitting ball" />
+        <div className="image-wrapper">
+          <img src={HittingImage} alt="Player hitting ball" />
+          <div className="review-tag">
+            <div className="review-images">
+              <img src={Testimony1} alt="" />
+              <img src={Testimony2} alt="" />
+              <img src={Testimony3} alt="" />
+            </div>
+            <span className="review-count">85+</span>
+            <span className="review-text">Reviews</span>
+          </div>
+        </div>
         <div className="schedule-wrapper">
           <div className="tag">Reserve your spot</div>
           <div className="schedule-info-wrapper">
             <h3>
-              Ready to improve your swing?
+              Ready to improve your game?
               <br />
               Book your first session today
             </h3>
